06f2f851432d37bdddb949635a94d1778813e66c
mstsurnalyakter <surnalyms@gmail.com>
2025-05-17 07:32:41 +0600

    refactor: update project structure and dependencies
    - Changed module type from CommonJS to ES Module in package.json.
    - Updated test script to use nodemon for automatic restarts during development.
    - Added dotenv, express, and mongoose as dependencies in package.json.
    - Created a constants.js file to store the database name.
    - Implemented initial setup in index.js for connecting to MongoDB and starting the Express server (currently commented out).

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+
+(async()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.log(error);
+        throw error;
+       })
+       app.listen(process.env.PORT,()=>{
+        console.log(`http://localhost:${process.env.PORT}`)
+       })
+    } catch (error) {
+        console.error("Error",error);
+        throw error;
+    }
+})()
+
+ */
